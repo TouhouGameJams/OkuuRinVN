15307263e38ac1282c2d97892976aa7216641b2b
Ckimchris <ckimchris@gmail.com>
2022-06-09 02:05:02 -0700

    Adding a fade screen and options gear

---
+++ Assets/Scripts/FadeScreen.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+using UnityEngine.UI;
+
+public class FadeScreen : MonoBehaviour
+{
+    CanvasRenderer elementToFade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elementToFade = gameObject.GetComponent<CanvasRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [YarnCommand("FadeOut")]
+    public void FadeOut()
+    {
+        StartCoroutine(FadeRoutine(0f, 1f));
+    }
+
+    [YarnCommand("FadeIn")]
+    public void FadeIn()
+    {
+        StartCoroutine(FadeRoutine(1f, 1f));
+    }
+
+    IEnumerator FadeRoutine(float endValue, float duration)
+    {
+        float time = 0;
+        float startValue = elementToFade.GetAlpha();
+        while (time < duration)
+        {
+            elementToFade.SetAlpha(Mathf.Lerp(startValue, endValue, time / duration));
+            time += Time.deltaTime;
+            yield return null;
+        }
+        elementToFade.SetAlpha(endValue);
+    }
+}
