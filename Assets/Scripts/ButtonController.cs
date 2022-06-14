a2db5839fa4a9579971fd71f2b8bc03a41c73f4f
Seeker <yoshimitsu12@outlook.com>
2022-06-14 11:07:12 +0900

    Create TitleScene

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
+    private Text m_text;
+
+    public void StartToClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
     public void OnClick()
     {
         SoundManager soundManager = SoundManager.Instance;
@@ -14,8 +22,18 @@ public class ButtonController : MonoBehaviour
 
     public void OnHover()
     {
+        m_text = GetComponentInChildren<Text>();
+        m_text.color = Color.red;
         SoundManager soundManager = SoundManager.Instance;
         soundManager.PlaySFX(soundManager.GetSFX("Hover"));
+
+    }
+
+    public void OnExit()
+    {
+        m_text = GetComponentInChildren<Text>();
+        m_text.color = Color.white;
+
     }
 
     public void ExitToClick()
