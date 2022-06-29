0581ab8ba2f15c280bb495700867e143675f28e2
Hai Lam Dang <hai.dang@ue-germany.de>
2022-06-29 02:09:16 +0200

    [FIX] Chireiden_Script

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -1,9 +1,8 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 
-public class CharacterManager : MonoBehaviour
+public class CharacterManager: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
